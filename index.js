893cf3ea20b412351ef31e49355d11b6b3ac1b6e
mdallamaiqbal <bd999plus13@gmail.com>
2026-06-23 10:22:25 +0600

    create arts api connect with front end

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
-const app = express()
+const cors = require('cors');
+const app = express();
 const uri = process.env.MONGO_DB_URI;
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -20,6 +24,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const database = client.db("art_hub_db");
+    const artCollection = database.collection("arts");
+    
+    app.post('/api/arts' , async(req,res)=>{
+        const art = req.body;
+        const result = await artCollection.insertOne(art);
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
