16b09efa5a7eac4c92f2b342832425a4e0ff2d8a
mdallamaiqbal <bd999plus13@gmail.com>
2026-06-23 09:52:06 +0600

    add mongodb

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express()
+const uri = process.env.MONGO_DB_URI;
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
